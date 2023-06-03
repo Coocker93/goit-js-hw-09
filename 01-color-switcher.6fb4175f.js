8c2a3d1c3d384c7f25f94046f1f2411a17be6db6
Coocker93 <122461768+Coocker93@users.noreply.github.com>
2023-06-03 10:27:26 +0000

    Deploying to gh-pages from @ Coocker93/goit-js-hw-09@a8db2a7b4b0ef909e3ed262b9eb06f9459f30016 🚀

---
+++ 01-color-switcher.6fb4175f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.querySelector("body");let n=null;t.addEventListener("click",(function(){n=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=t}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.6fb4175f.js.map
